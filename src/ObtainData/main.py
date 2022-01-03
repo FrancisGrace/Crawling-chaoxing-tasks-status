5eb3644eac4a6bf741e7e1a63615ea8495c81ff0
LinXuan <hou_xiaoxuan@qq.com>
2022-01-04 00:55:10 +0800

    登陆逻辑

---
+++ src/ObtainData/main.py
@@ -0,0 +1,8 @@
+'''
+Author: LinXuan
+Date: 2022-01-03 23:37:18
+Description: 获取数据的主程序，控制程序逻辑
+LastEditors: LinXuan
+LastEditTime: 2022-01-03 23:37:18
+FilePath: /opensource-homework/src/ObtainData/main.py
+'''
